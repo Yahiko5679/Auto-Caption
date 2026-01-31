3f3570bc3d0c8feccd77a5c9340441477aa692c6
Yahiko5679 <yahiko5679@gmail.com>
2026-01-31 14:43:17 +0530

    Update command.py

--- Rkn_Botz/command.py
+++ Rkn_Botz/command.py
@@ -1,21 +1,12 @@
 from pyrogram import Client, filters, errors, types
 from pyrogram.types import Message
-from bot import Bot
-from keyboards import start_buttons
+from config import MythicBots
+from .callbacks import start_buttons
 
 @Client.on_message(filters.command("help") & filters.private)
-async def help_cmd(_, message: Message):
+async def help_cmd(client, message: Message):
     await message.reply_text(
-        """
-🆘 <b>Help Menu</b>
-
-• Send any video / document  
-• Bot auto adds caption  
-• Supports HTML formatting  
-• Fast & simple  
-
-Use buttons below 👇
-        """,
+        MythicBots.HELP_TEXT,
         reply_markup=start_buttons(),
         disable_web_page_preview=True
     )
